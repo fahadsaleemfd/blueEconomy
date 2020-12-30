2487f138cb0a618bab72f308e0d6401f0ef6aa73
fahadsaleem1 <igz.iwd.25@gmail.com>
2020-12-30 14:55:41 +0500

    add

--- src/pages/blue_economy.js
+++ src/pages/blue_economy.js
@@ -10,13 +10,13 @@ import {Helmet} from "react-helmet";
 import { StaticQuery, graphql } from "gatsby"
 import { node } from "prop-types"
 
-const Blueeconomy = () => {
+const Blueeconomy = () => (
     
-    
-            
-      
-  
-        return (  
+  <StaticQuery query={Bluequery} render={data=>{
+
+          // const fetchData = data.allMarkdownRemark.edges
+          console.log(data.list_section.edges)
+          return (  
         <div>
             <Layout>
                 <SEO title="Be Challenge 2021" />            
@@ -48,49 +48,69 @@ const Blueeconomy = () => {
         </section>
 
 
-        <section id="about" class="section-1 highlights team image-right">
-            <div class="container">
+        <section id="about" class="section-1 highlights team image-right" style={{padding:"50px 0"}}>
+          
+       
+        
+          <div class="container">
+            {data.list_section.edges.map((tag,index) => (
+              <div>
                 <div class="row">
                     <div class="col-12 col-lg-8 align-self-top text" style={{textAlign:"center"}}>
                         <div class="row intro m-0">
                             <div class="col-12 p-0">
                                 
-                                <h2>Title</h2>
+                                <h2>{tag.node.frontmatter.title}</h2>
 
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col-12 p-0 pr-md-5" style={{textAlign:"left"}}>
-                               <p>demo title demo title demo title demo title demo title demo title demo title demo title demo title demo title </p>
+                            <div class="col-12 p-0 pr-md-5" style={{textAlign:"justify"}}>
+                               <p>{tag.node.frontmatter.description}</p>
                             </div>
                         </div>
                     </div>
                     <div  class="col-12 col-lg-4">
                               
-                                <img src={bkImage} class="fit-image" />    
+                                <img src={tag.node.frontmatter.image} class="fit-image" />    
                     </div>
                 </div>
+                <br></br><br></br><br></br>
+                </div>
+                ))}
             </div>
+           
+        
+          
         </section>
             
             </div>
         )
- 
-        }
+      }}/>
+)
 
   const Bluequery = graphql`
-  query Bluequerindex{
-    allMarkdownRemark(filter: {fileAbsolutePath: {regex: "posts/business/"}} 
+  {
+    video_sections: allMarkdownRemark(filter: {fileAbsolutePath: {regex: "posts/blueeconomy/video_section/"}}
     ) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+        }
+      }
+    }
+    
+    list_section: allMarkdownRemark(filter: {fileAbsolutePath: {regex: "posts/blueeconomy/list_section/"}}
+    )  {
       edges {
         node {
           id
           frontmatter {
+            
             title
             description
-            date
-            projectLink
-            link
             image
           }
           fields{
@@ -100,9 +120,11 @@ const Blueeconomy = () => {
         }
       }
     }
+
+
   }
-  
-   `
+
+ `
 
 
 
