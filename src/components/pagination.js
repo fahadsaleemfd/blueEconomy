35eccf7b13d969f28461e5270f6b739b624bdaea
fahadsaleem1 <igz.iwd.25@gmail.com>
2020-12-31 18:30:21 +0500

    add

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -4,38 +4,11 @@ import { withPrefix ,Link } from "gatsby"
 
 const Pagination = ({prevPage,nextPage,isFirst, isLast, currentPage, numPages}) => {
 
-    
+            console.log(isFirst)
  
         return (  
 
-            // <div class="row">
-            //             <div class="col-12">
-            //                 <nav>
-
-            //                 <ul class="pagination justify-content-center">
-                                  
-            //                                 <li class="page-item">
-                                        
-            //                                     <Link class="page-link">
-            //                                                 <i class="fas fa-angle-left"></i>
-            //                                     </Link>
-                                                
-            //                                 </li>
-                                   
-            //                         <li class="page-item"><a class="page-link" href="#">4</a></li>
-                                   
-            //                             <li class="page-item">
-            //                                  <Link class="page-link"> 
-            //                                     <i class="fas fa-angle-right"></i>
-            //                                  </Link> 
-            //                             </li>
-                                   
-
-            //                     </ul>
-                                        
-            //                 </nav>
-            //             </div>
-            //         </div> 
+            
 
 
              <div class="row">
@@ -43,7 +16,7 @@ const Pagination = ({prevPage,nextPage,isFirst, isLast, currentPage, numPages}) 
                             <nav>
 
                             <ul class="pagination justify-content-center">
-                                    {!isFirst && (
+                                    {!isFirst &&  ( 
                                             <li class="page-item">
                                         
                                                 <Link class="page-link" to={'/incubation/'+prevPage}>
@@ -51,15 +24,15 @@ const Pagination = ({prevPage,nextPage,isFirst, isLast, currentPage, numPages}) 
                                                 </Link>
                                                 
                                             </li>
-                                     )}
+                                      )}
                                     <li class="page-item"><a class="page-link" href="#">{currentPage}/{numPages}</a></li>
-                                    {!isLast && (
+                                    {/* {!isLast && ( */}
                                         <li class="page-item">
                                             <Link class="page-link" to={'/incubation/' + nextPage}>
                                                 <i class="fas fa-angle-right"></i>
                                             </Link>
                                         </li>
-                                    )}
+                                    {/* )} */}
 
                                 </ul>
                                         
