30a64b4041744c7577cd7b012bd9df7fe2c0b4f1
fahadsaleem1 <igz.iwd.25@gmail.com>
2021-01-04 14:51:45 +0500

    add

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
+import { StaticQuery,Link } from "gatsby"
 import bkImage from "../dist/assets/images/about-5.jpg"
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -8,7 +8,13 @@ import {Helmet} from "react-helmet";
 import Videofile from "../dist/assets/file.mp4"
 
 const IndexPage = () => (
-  
+
+  <StaticQuery query={Homepage} render={data=>{
+
+    console.log(data)
+
+
+  return (  
   <Layout>
     <SEO title="Home" />
     <section class="hero p-0 odd">
@@ -19,10 +25,7 @@ const IndexPage = () => (
                     <div class="swiper-slide slide-center">
 
                     <video loop autoPlay class="full-image" data-mask="40">
-                            {/* <source src={data.video_sections.edges[0].node.frontmatter.video} type="video/mp4"/>
-                             */}
-                             Videofile
-                                <source src={Videofile} type="video/mp4"/>
+                             <source src={data.allMarkdownRemark.edges[0].node.frontmatter.video} type="video/mp4"/> */}
                             
                     </video>
                         {/* <img src="assets/images/bg-2.jpg" alt="Full Image" class="full-image" data-mask="40"/>   */}
@@ -32,8 +35,8 @@ const IndexPage = () => (
                                 <div class="center text-left text-md-center">
 
                                  
-                                    {/* <h2 >{data.video_sections.edges[0].node.frontmatter.title}</h2>
-                                    <p>{data.video_sections.edges[0].node.frontmatter.description}</p> */}
+                                    <h2 >{data.allMarkdownRemark.edges[0].node.frontmatter.title}</h2>
+                                    
                                   
                                 </div>
                             </div>
@@ -47,23 +50,23 @@ const IndexPage = () => (
 <section id="about" class="section-1 highlights team image-right" style={{padding:"50px 0"}}>
         
         <div class="container">
-          {/* {data.list_section.edges.map((tag,index) => ( */}
+         
             <div>
               <div class="row">
                   <div class="col-12 col-lg-12 align-self-top text" style={{textAlign:"center"}}>
                       <div class="row intro m-0">
                           <div class="col-12 p-0">
                               
-                              {/* <h2>{tag.node.frontmatter.title}</h2> */}Title
+                              <h2>{data.allMarkdownRemark.edges[0].node.frontmatter.section1title}</h2>
 
                           </div>
                       </div>
                       <div class="row">
                           <div class="col-12 col-lg-12 p-0 pr-md-5" style={{textAlign:"justify"}}>
-                             {/* <p>{tag.node.frontmatter.description}</p> */}
+                            
+                             <div dangerouslySetInnerHTML={{__html:data.allMarkdownRemark.edges[0].node.html}}></div>
 
-                             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                             title 2
+                            
                           </div>
                       </div>
                   </div>
@@ -71,7 +74,7 @@ const IndexPage = () => (
               </div>
               <br></br><br></br><br></br>
               </div>
-              {/* ))} */}
+      
           </div>
          
       </section>
@@ -79,27 +82,27 @@ const IndexPage = () => (
       <section id="about" class="section-1 highlights team image-right" style={{padding:"50px 0"}}>
         
         <div class="container">
-          {/* {data.list_section.edges.map((tag,index) => ( */}
+        
             <div>
               <div class="row">
                   <div class="col-12 col-lg-8 align-self-top text" style={{textAlign:"center"}}>
                       <div class="row intro m-0">
                           <div class="col-12 p-0">
                               
-                              {/* <h2>{tag.node.frontmatter.title}</h2> */}
+                              <h2>{data.allMarkdownRemark.edges[0].node.frontmatter.section2title}</h2>
 
                           </div>
                       </div>
                       <div class="row">
                           <div class="col-12 p-0 pr-md-5" style={{textAlign:"justify"}}>
-                             {/* <p>{tag.node.frontmatter.description}</p> */}
+                             <p>{data.allMarkdownRemark.edges[0].node.frontmatter.section2description}</p>
                           </div>
                       </div>
                   </div>
                   <div  class="col-12 col-lg-4">
                             
-                              {/* <img src={tag.node.frontmatter.image} class="fit-image" />     */}
-                              <img src={bkImage} class="fit-image" />  
+                              <img src={data.allMarkdownRemark.edges[0].node.frontmatter.section2image} class="fit-image" />    
+                             
                   </div>
               </div>
               <br></br><br></br><br></br>
@@ -118,6 +121,32 @@ const IndexPage = () => (
 
   </Layout>
   
+  )
+}}/>
 )
-
+export const Homepage = graphql`
+  query homepagequery{
+    allMarkdownRemark(filter: {fileAbsolutePath: {regex: "posts/homepage/"}} 
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            video
+            section1title
+            section2title
+            section2description
+            section2image
+            image
+          }
+          html
+          excerpt
+        }
+      }
+      
+    }
+  }
+  
+   `
 export default IndexPage
