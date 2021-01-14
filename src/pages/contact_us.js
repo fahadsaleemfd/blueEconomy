a78ab3c88ab285fe86ad929d427971668ad2b770
fahadsaleem1 <igz.iwd.25@gmail.com>
2021-01-14 15:17:10 +0500

    add

--- src/pages/contact_us.js
+++ src/pages/contact_us.js
@@ -8,7 +8,7 @@ import axios from "axios"
 import * as qs from "query-string"
 
 class ContactFormPage extends React.Component {
-    
+
     constructor(props) {
         super(props)
         this.domRef = React.createRef()
@@ -51,23 +51,119 @@ class ContactFormPage extends React.Component {
       }
   
     render() {
+       
       return (
-       <>
-          <h1>Contact</h1>
-  
-          {this.state.feedbackMsg && <p>{this.state.feedbackMsg}</p>}
-  
-          <form ref={this.domRef} name="Contact Form" method="POST" data-netlify="true" onSubmit={event => this.handleSubmit(event)}>
-            <input ref="form-name" type="hidden" name="form-name" value="Contact Form" />
-          
-            <input ref="email" type="email" name="email" />
+          <div>
+                <StaticQuery query={contactquery} render={data=>{
+                   
+                return ( <div>   
+                     
+                <Helmet>
+                        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDI5R6l_RO_28-77Ah0uDc3p-U3qRr67Ls&callback=initMap" async defer />
+                        var map = new google.maps.Map(document.getElementById("googleMap"),mapProp)
+                </Helmet>
+                 <Layout>
+                      <SEO title="Be Challenge 2021" />            
+                      <section class="section-2">
+            <div class="container">
+                <h2 style={{textAlign:"center"}}>CONTACT US</h2>
+                <div class="row">
+                    
+                    
+                    <div class="col-12 col-lg-6">
+                         <div class="row form-group-margin">
+
+                                <div class="col-12 col-md-6 col-lg-2 m-0 p-2 input-group">
+                                     <i class="fa fa-map-marker" aria-hidden="true"></i>
+                                </div>
+
+                                <div class="col-12 col-md-6 col-lg-8 m-0 p-2 input-group">
+                                     <p>{data.allMarkdownRemark.edges[0].node.frontmatter.address}</p>
+                                </div>
+
+                            </div>
+
+                            <div class="row form-group-margin">
+
+                                    <div class="col-12 col-md-6 col-lg-2 m-0 p-2 input-group">
+                                    <i class="fa fa-phone" aria-hidden="true"></i>
+                                    </div>
+
+                                    <div class="col-12 col-md-6 col-lg-8 m-0 p-2 input-group">
+                                        <p>{data.allMarkdownRemark.edges[0].node.frontmatter.phone_number}</p>
+                                       
+                                    </div>
+
+                            </div>
+
+                            <div class="row form-group-margin">
+
+                                    <div class="col-12 col-md-6 col-lg-2 m-0 p-2 input-group">
+                                           <i class="fa fa-envelope" aria-hidden="true"></i>
+
+                                    </div>
+
+                            <div class="col-12 col-md-6 col-lg-8 m-0 p-2 input-group">
+                            <p>{data.allMarkdownRemark.edges[0].node.frontmatter.email}</p>
+                            </div>
+
+                            </div>
+                    </div>
+
+                    <div class="col-12 col-lg-6">
+                    {this.state.feedbackMsg && <p>{this.state.feedbackMsg}</p>}
+                    <form ref={this.domRef} name="Contact Form" method="POST" data-netlify="true" onSubmit={event => this.handleSubmit(event)}>
+                            <input ref="form-name" type="hidden" name="form-name" value="Contact Form" />
+                            
+                            <div class="row form-group-margin">
+                                <div class="col-12 col-md-6 col-lg-6 m-0 p-2 input-group">
+                                    <input type="text" name="name" class="form-control field-name" placeholder="Name"/>
+                                </div>
+                                <div class="col-12 col-md-6 col-lg-6 m-0 p-2 input-group">
+                                    <input type="text" name="last_name" class="form-control field-email" placeholder="Last Name"/>
+                                </div>
+                                <br></br>
+                                <div class="col-12 m-0 p-2 input-group">
+                                    <input type="email" name="email" class="form-control field-phone" placeholder="Email"/>
+                                </div>
+                                <div class="col-12 m-0 p-2 input-group">
+                                    <textarea name="message" class="form-control field-message" placeholder="Message"></textarea>
+                                </div>
+                                <div class="col-12 col-12 m-0 pl-md-2">
+                                    <span class="form-alert"></span>
+                                </div>
+                                <div class="col-12 input-group m-0 p-2">
+                                <button class="btn primary-button" type="submit">SEND</button>
+                                    
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="section-3 odd" style={{background:"black"}}>
             
-            <textarea ref="message" name="message" />
-            <button type="submit">Send</button>
-          </form>
-      </>
-      )
-    }
+                <div class="container">
+                    <div class="row">
+                        <SimpleMap/>
+                    </div>
+                </div>
+                
+            
+        </section>
+
+
+                 </Layout>  
+            </div>)
+         
+            }}/> 
+        </div> )
+
+    
+     
+      }
   }
   
 
